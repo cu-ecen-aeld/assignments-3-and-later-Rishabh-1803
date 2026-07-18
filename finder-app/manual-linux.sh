47c75fe3052965930d60c141cce0369a42fe86b9
Rishabh-1803 <rishabh180306@gmail.com>
2026-07-18 19:47:11 +0530

    Complete assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -6,75 +6,196 @@ set -e
 set -u
 
 OUTDIR=/tmp/aeld
-KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
+KERNEL_REPO=https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
 then
-	echo "Using default directory ${OUTDIR} for output"
+    echo "Using default directory ${OUTDIR} for output"
 else
-	OUTDIR=$1
-	echo "Using passed directory ${OUTDIR} for output"
+    OUTDIR=$1
+    echo "Using passed directory ${OUTDIR} for output"
 fi
 
 mkdir -p ${OUTDIR}
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+#################################################
+# Build Linux Kernel
+#################################################
 
-    # TODO: Add your kernel build steps here
-fi
+cd ${OUTDIR}
 
-echo "Adding the Image in outdir"
-
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
+if [ ! -d "${OUTDIR}/linux-stable" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    echo "Cloning Linux kernel ${KERNEL_VERSION}"
+    git clone ${KERNEL_REPO} \
+        --depth 1 \
+        --single-branch \
+        --branch ${KERNEL_VERSION}
 fi
 
-# TODO: Create necessary base directories
+if [ ! -f "${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image" ]
+then
+    cd linux-stable
 
-cd "$OUTDIR"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    make -j$(nproc) \
+        ARCH=${ARCH} \
+        CROSS_COMPILE=${CROSS_COMPILE}
+fi
+
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+
+#################################################
+# RootFS Creation
+#################################################
+
+echo "Creating root filesystem"
+
+sudo rm -rf ${OUTDIR}/rootfs
+
+mkdir -p ${OUTDIR}/rootfs
+
+cd ${OUTDIR}/rootfs
+
+mkdir -p \
+bin \
+dev \
+etc \
+home \
+lib \
+lib64 \
+proc \
+sbin \
+sys \
+tmp \
+usr/bin \
+usr/lib \
+usr/sbin \
+var/log
+
+#################################################
+# Busybox
+#################################################
+
+cd ${OUTDIR}
+
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-else
-    cd busybox
+    git clone https://git.busybox.net/busybox
 fi
 
-# TODO: Make and install busybox
+cd busybox
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+git checkout 1_33_1
 
-# TODO: Add library dependencies to rootfs
+make distclean || true
 
-# TODO: Make device nodes
+make ARCH=${ARCH} \
+     CROSS_COMPILE=${CROSS_COMPILE} \
+     defconfig
 
-# TODO: Clean and build the writer utility
+make -j$(nproc) \
+     ARCH=${ARCH} \
+     CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+make CONFIG_PREFIX=${OUTDIR}/rootfs \
+     ARCH=${ARCH} \
+     CROSS_COMPILE=${CROSS_COMPILE} \
+     install
 
-# TODO: Chown the root directory
+#################################################
+# Shared Libraries
+#################################################
 
-# TODO: Create initramfs.cpio.gz
+mkdir -p ${OUTDIR}/rootfs/lib
+mkdir -p ${OUTDIR}/rootfs/lib64
+
+cp -L /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 \
+      ${OUTDIR}/rootfs/lib/
+
+cp -L /usr/aarch64-linux-gnu/lib/libc.so.* \
+      ${OUTDIR}/rootfs/lib/
+
+cp -L /usr/aarch64-linux-gnu/lib/libm.so.* \
+      ${OUTDIR}/rootfs/lib/
+
+cp -L /usr/aarch64-linux-gnu/lib/libresolv.so.* \
+      ${OUTDIR}/rootfs/lib/
+
+#################################################
+# Device Nodes
+#################################################
+
+sudo mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3 || true
+sudo mknod -m 600 ${OUTDIR}/rootfs/dev/console c 5 1 || true
+
+#################################################
+# Build Writer
+#################################################
+
+cd ${FINDER_APP_DIR}
+
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
+
+#################################################
+# Copy Assignment Files
+#################################################
+
+mkdir -p ${OUTDIR}/rootfs/home/conf
+
+cp ${FINDER_APP_DIR}/finder.sh \
+    ${OUTDIR}/rootfs/home/
+
+cp ${FINDER_APP_DIR}/finder-test.sh \
+    ${OUTDIR}/rootfs/home/
+
+cp ${FINDER_APP_DIR}/writer \
+    ${OUTDIR}/rootfs/home/
+
+cp ${FINDER_APP_DIR}/autorun-qemu.sh \
+    ${OUTDIR}/rootfs/home/
+
+cp ${FINDER_APP_DIR}/conf/assignment.txt \
+    ${OUTDIR}/rootfs/home/conf/
+
+cp ${FINDER_APP_DIR}/conf/username.txt \
+    ${OUTDIR}/rootfs/home/conf/
+
+chmod +x ${OUTDIR}/rootfs/home/finder.sh
+chmod +x ${OUTDIR}/rootfs/home/finder-test.sh
+chmod +x ${OUTDIR}/rootfs/home/writer
+chmod +x ${OUTDIR}/rootfs/home/autorun-qemu.sh
+
+#################################################
+# Ownership
+#################################################
+
+sudo chown -R root:root ${OUTDIR}/rootfs
+
+#################################################
+# Create initramfs
+#################################################
+
+cd ${OUTDIR}/rootfs
+
+find . | cpio -H newc -ov --owner root:root \
+    > ${OUTDIR}/initramfs.cpio
+
+cd ${OUTDIR}
+
+gzip -f initramfs.cpio
+
+echo ""
+echo "Build complete"
+echo "Kernel Image:"
+echo "${OUTDIR}/Image"
+echo ""
+echo "Initramfs:"
+echo "${OUTDIR}/initramfs.cpio.gz"
